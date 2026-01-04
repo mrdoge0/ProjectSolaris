61fbbb0bb7c72b7ff6519b701f42ddc70a7d76df
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2026-01-04 17:10:01 +0300

    Create post-fs-hook.sh

---
+++ rom-patchsets/main/add-or-replace/system/solaris/post-fs-hook.sh
@@ -0,0 +1,25 @@
+#!/system/bin/sh
+# Project Solaris post-fs hook
+
+# Inherit solaris.conf
+source /system/solaris/solaris.conf
+#SPOOF_PROPS='true'
+#BRAND='Nothing'
+#MANUFACTURER="${BRAND}"
+#DEVICE='Spacewar'
+#MODEL='A063'
+#MARKETNAME='Nothing Phone (1)'
+#FINGERPRINT='Nothing/Spacewar/Spacewar:15/AQ3A.240929.001/2512191652:user/release-keys'
+
+# Resetprop path
+RP_BIN='/system/solaris/resetprop'
+
+# Resetprops
+if [ "${SPOOF_PROPS}" == 'true' ]; then
+  "${RP_BIN}" -n 'ro.product.brand' "${BRAND}"
+  "${RP_BIN}" -n 'ro.product.manufacturer' "${MANUFACTURER}"
+  "${RP_BIN}" -n 'ro.product.device' "${DEVC}"
+  "${RP_BIN}" -n 'ro.product.model' "${MODL}"
+  "${RP_BIN}" -n 'ro.product.brand_device_name' "${MRNM_FULL}"
+  "${RP_BIN}" -n 'ro.build.fingerprint' "${FINGERPRINT}"
+fi
