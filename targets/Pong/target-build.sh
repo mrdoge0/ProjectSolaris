030c4e85ab425827ec08e7705acf9df73e0f80e7
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2026-01-11 01:14:40 +0300

    Add target-build.sh for Pong

---
+++ targets/Pong/target-build.sh
@@ -0,0 +1,22 @@
+# no shebang, this is meant to be sourced in build time
+# Build target - Nothing Phone (2) (Pong)
+# (c) 2026 mrdoge0 and the Project Solaris contributors, Free Software Licensed under GPLv3.
+
+# Download full images for Pong
+dl_images latest Pong all
+
+# Since Pong is natively supported, it's zero problem to make a fully native mod
+use_system_images latest Pong
+use_vendor_images latest Pong
+
+# Extract system and it's splits
+extract_to_work erofs
+
+# Install main patchset which is what makes Project Solaris, Project Solaris
+install_patchset main
+
+# Install generic solaris.conf for native builds
+install_patchset native-mod
+
+# Package system and it's splits to EROFS images
+package_work erofs
