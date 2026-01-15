2b1bc688c3d2cde75fd65e16f6d598e35d43b171
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2026-01-16 01:33:03 +0300

    Create BOOTSTRAP.sh

---
+++ BOOTSTRAP.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# bootstrap the source
+
+if [ -f "./source_root.attr_override.list" ]; then
+  cat "./source_root.attr_override.list" | while IFS= read -r TARGET_LINE; do
+    TGT_FILE_OR_DIR="$(echo "${TARGET_LINE}" | cut -d' ' -f1)"
+    TGT_ATTR="$(echo "${TARGET_LINE}" | cut -d' ' -f2)"
+    chmod -Rv "${TGT_ATTR}" "./${TGT_FILE_OR_DIR}" || exit 1
+  done
+else
+  echo 'Always run ALL scripts of this project while your PWD is the root of the cloned repository.'
+  exit 1
+fi
+
+exit 0
