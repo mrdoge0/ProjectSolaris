c9e71b6cf46372adad95e0f82b02aff0c2c6f371
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2026-01-22 00:34:25 +0300

    Adapt to the new attribute override syntax

--- BOOTSTRAP.sh
+++ BOOTSTRAP.sh
@@ -4,7 +4,7 @@
 if [ -f "./.PROJECTSOLARIS_SOURCE_MARK" ] && [ -f "./source_root.attr_override.list" ]; then
   cat "./source_root.attr_override.list" | while IFS= read -r TARGET_LINE; do
     TGT_FILE_OR_DIR="$(echo "${TARGET_LINE}" | cut -d' ' -f1)"
-    TGT_ATTR="$(echo "${TARGET_LINE}" | cut -d' ' -f2)"
+    TGT_ATTR="$(echo "${TARGET_LINE}" | cut -d' ' -f4)"
     chmod -Rv "${TGT_ATTR}" "./${TGT_FILE_OR_DIR}" || exit 1
   done
 else
