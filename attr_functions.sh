8d36da6474fb5db998cd873963a536313a3bd5bf
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2026-02-11 23:36:24 +0300

    Initial

---
+++ attr_functions.sh
@@ -0,0 +1,11 @@
+# no shebang, this is meant to be sourced
+# Attribute and SELinux functions
+# (c) 2026 mrdoge0 and the Project Solaris contributors, Free Software Licensed under GPLv3.
+
+## 
+## INSTALL ATTR_OVERRIDE
+## 
+install_attr_override() {
+  # Syntax:
+  # install_attr_override [mountpoint] [source_override] [target_fs] [target_fsconf]
+}
