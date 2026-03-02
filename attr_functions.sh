6961873e5da3b1ea86243c486918382499f2ba6e
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2026-03-02 20:36:23 +0300

    Some devel at install_attr_override

--- attr_functions.sh
+++ attr_functions.sh
@@ -7,5 +7,30 @@
 ## 
 install_attr_override() {
   # Syntax:
-  # install_attr_override [mountpoint] [source_override] [target_fs] [target_fsconf]
+  # install_attr_override [mountpoint] [source_override] [target_fsconf]
+
+  # Declare vars
+  MOUNTPOINT="${1}"
+  OVERRIDE="${2}"
+  TARGET_FSCONF="${3}"
+
+  # If TARGET_FSCONF is N/A
+  if [ -z "${TARGET_FSCONF}" ]; then
+    TARGET_FSCONF='na'
+  fi
+
+  # Cook it
+  cat "${OVERRIDE}" | while IFS= read -r LINE; do
+    if [ "${TARGET_FSCONF}" == 'na' ]; then
+      TFILE="$(echo "${LINE}" | cut -d' ' -f1)"
+      TUID="$(echo "${LINE}" | cut -d' ' -f2)"
+      TGID="$(echo "${LINE}" | cut -d' ' -f3)"
+      TPERM="$(echo "${LINE}" | cut -d' ' -f4)"
+      if [ "${TUID}" != 'na' ] && [ "${TGID}" != 'na' ]; then
+        chown -v ${TUID}:${TGID} "${MOUNTPOINT}/${TFILE}"
+      fi
+      chmod -v ${TPERM} "${MOUNTPOINT}/${TFILE}"
+    else
+    fi
+  done
 }
